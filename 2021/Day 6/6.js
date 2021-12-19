395abfed1c5b82f01b40dcc4af2a43b0eb540ef0
matthewkhle <47379008+matthewkhle@users.noreply.github.com>
2021-12-18 21:06:27 -0800

    Day 6 Part 1 complete
    - removed array split

--- 2021/Day 6/6.js
+++ 2021/Day 6/6.js
@@ -10,37 +10,16 @@ for (let i = 0; i < ages.length; i++) {
 // console.log(ages)
 let ages2 = []
 
-for (let day = 0; day < 256; day++) {
+for (let day = 0; day < 80; day++) {
 	let originalLength = ages.length
 	for (let i = 0; i < originalLength; i++) {
-		if (ages.length < 150) {
-			if (ages[i] == 0) {
-				ages[i] = 6
-				ages.push(8)
-			} else {
-				ages[i]--
-			}
+		if (ages[i] == 0) {
+			ages[i] = 6
+			ages.push(8)
 		} else {
-			if (ages[i] == 0) {
-				ages[i] = 6
-				ages2.push(8)
-			} else {
-				ages[i]--
-			}
+			ages[i]--
 		}
 	}
-
-	originalLength = ages2.length
-	for (let i = 0; i < originalLength; i++) {
-		if (ages2[i] == 0) {
-			ages2[i] = 6
-			ages2.push(8)
-		} else {
-			ages2[i]--
-		}
-	}
-
-	console.log(day)
 }
 
 console.log(ages.length + ages2.length)
